43808fa6bf3f3f1cf9fe5b6905be034a5daf5590
Thodoris Chatziioannidis <hthodoris@gmail.com>
2018-08-31 01:24:36 +0300

    various customizations

--- exec_or_focus.py
+++ exec_or_focus.py
@@ -2,23 +2,25 @@ import sys
 import subprocess
 import time
 
+
 def focus(win_class):
     i3msg = ["i3-msg", "[class=\"{0}\"] focus".format(win_class)]
     t = subprocess.Popen(i3msg, stdout=subprocess.PIPE)
     response = t.stdout.read()
     return response
 
+
 def execute (command):
     i3msg = ["i3-msg", "exec","--no-startup-id" ,"{0}".format(command)]
     t = subprocess.Popen(i3msg, stdout=subprocess.PIPE)    
+
 
 SUCC_MSG = b'[{"success":true}]\n'
 if len(sys.argv) < 3:
     print("Usage: <window class> <command>")
     exit(1)
-
 response = focus(sys.argv[1])
-print(sys.argv[1])    
+print(sys.argv[1])
 
 
 print(response)
